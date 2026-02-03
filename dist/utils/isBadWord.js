07da881657df89cd6f6630c9f73a7c3e6ea50b47
Paul Stevens <paul.stevens.developer@gmail.com>
2026-02-02 17:57:44 -0800

    chore: remove unused dist files and middleware; add tsconfig.json for TypeScript configuration; update package dependencies

--- dist/utils/isBadWord.js
+++
@@ -1,8 +0,0 @@
-import leoProfanity from 'leo-profanity';
-import { Filter } from 'bad-words';
-const filter = new Filter();
-export default (word) => {
-    const dirtyWord = leoProfanity.check(word);
-    const badWord = filter.isProfane(word);
-    return dirtyWord || badWord;
-};
--- dist/utils/swagger/exportSwagger.js
+++
@@ -1,7 +0,0 @@
-import { swaggerSpec } from './swaggerSpec.js';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-fs.writeFileSync(path.join(__dirname, '../../docs/openapi.json'), JSON.stringify(swaggerSpec, null, 2));
-console.log('OpenAPI spec exported to docs/openapi.json');
